1b89640433c42d19b5a21a91340ab9016511cbc4
ibnu maulana <45716669+ibnumaulana215@users.noreply.github.com>
2018-12-16 18:49:39 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=1;
+    while(a<10)
+    {
+        cout<<a<<endl;
+        a++;
+    }
+
+    cout<<endl<<"---------------------------------------"<<endl;
+
+    int b=0;
+    do
+    {
+        cout<<b<<endl;
+        b++;
+    }
+    while(b<10);
+}
+
